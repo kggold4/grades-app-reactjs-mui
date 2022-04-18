e8170a54e88bd2c7b627bc0d7cd025518adf8b52
kggold4 <68167734+kggold4@users.noreply.github.com>
2022-04-19 00:40:44 +0300

    update blur functions

--- ex2_utils.py
+++ ex2_utils.py
@@ -82,10 +82,9 @@ def blurImage2(in_image: np.ndarray, k_size: int) -> np.ndarray:
     :param k_size: Kernel size
     :return: The Blurred image
     """
-    sigma = 0.3 * ((k_size - 1) * 0.5 - 1) + 0.8
-    guassian = cv2.getGaussianKernel(ksize=k_size, sigma=sigma)
-    guassian = guassian * guassian.transpose()
-    return cv2.filter2D(in_image, -1, guassian, borderType=cv2.BORDER_REPLICATE)
+    sigma = int(round(0.3 * ((k_size - 1) * 0.5 - 1) + 0.8))
+    kernel = cv2.getGaussianKernel(k_size, sigma)
+    return cv2.filter2D(in_image, -1, kernel, borderType=cv2.BORDER_REPLICATE)
 
 
 def edgeDetectionZeroCrossingSimple(img: np.ndarray) -> np.ndarray:
@@ -121,8 +120,75 @@ def edgeDetectionZeroCrossingLOG(img: np.ndarray) -> np.ndarray:
     :param img: Input image
     :return: Edge matrix
     """
-    blur = blurImage2(img, np.array([3, 3]))
+    # blur = blurImage2(img, np.array([3, 3]))
+    blur = blurImage2(img, 3)
     return edgeDetectionZeroCrossingSimple(blur)
+
+
+def edgeDetectionCanny(img: np.ndarray, thrs_1: float, thrs_2: float) -> (np.ndarray, np.ndarray):
+    """
+    Detecting edges usint "Canny Edge" method
+    :param img: Input image
+    :param thrs_1: T1
+    :param thrs_2: T2
+    :return: opencv solution, my implementation
+    """
+    opencv = cv2.Canny((img*255).astype(np.uint8), img.shape[0], img.shape[1])
+
+    if img.max() > 2:
+        img = img / 255
+    # get answer from sobel and normalize
+    G_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    G_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    img = ((G_x ** 2) + (G_y ** 2)) ** 0.5
+
+    # get directions from 0 to 180 and Quantize to 4 values
+    directions = (np.arctan2(G_x, G_y) * 180) / np.pi
+    directions %= 180
+    directions[(directions >= 0) & (directions < 22.5)] = 0
+    directions[(directions >= 157.5) & (directions < 180)] = 0
+    directions[(directions >= 22.5) & (directions < 67.5)] = 45
+    directions[(directions >= 67.5) & (directions < 112.5)] = 90
+    directions[(directions >= 112.5) & (directions < 157.5)] = 135
+
+    # NMS
+    for i in range(1, img.shape[0] - 1):
+        for j in range(1, img.shape[1] - 1):
+            nei_l = 0
+            nei_r = 0
+            if directions[i, j] == 0:
+                nei_l = img[i - 1, j]
+                nei_r = img[i + 1, j]
+            elif directions[i, j] == 45:
+                nei_l = img[i - 1, j - 1]
+                nei_r = img[i + 1, j + 1]
+            elif directions[i, j] == 90:
+                nei_l = img[i, j - 1]
+                nei_r = img[i, j + 1]
+            elif directions[i, j] == 135:
+                nei_l = img[i - 1, j + 1]
+                nei_r = img[i + 1, j - 1]
+            if (img[i, j] < nei_l) | (img[i, j] < nei_r):
+                img[i, j] = 0
+    # value of 1 to pixels that are greater then T1
+    # and 0 to pixels that smaller then T2
+    img[img >= thrs_1] = 1
+    img[img <= thrs_2] = 0
+    # if the pixel value is greater then T2 and smaller then T1 mark by value 3.
+    img[(thrs_2 < img) & (img < thrs_1)] = 3
+
+    for i in range(1, img.shape[0] - 1):
+        for j in range(1, img.shape[1] - 1):
+            if img[i, j] == 3:
+                #  if the pixel value is 3 check if one of his neighbors is 1
+                if 1 in img[i - 1:i + 2, j - 1:j + 2]:
+                    img[i, j] = 1
+                else:
+                    img[i, j] = 0
+    # if there is any left marks change to 0. (usually at borders)
+    img[img == 3] = 0
+    img = (img * 255).astype(np.uint8)
+    return opencv, img
 
 
 def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
@@ -135,57 +201,37 @@ def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
     :return: A list containing the detected circles,
                 [(x,y,radius),(x,y,radius),...]
     """
-    # use this sorce: https://github.com/PavanGJ/Circle-Hough-Transform/blob/master/main.py
-    #  Use the Canny Edge detector as the edge detector
-    height, width = img.shape
-    img = cv2.GaussianBlur(img, (5, 5), 0)
-    img = cv2.Canny(img, 50, 100)
-    # detect all edges
-    edges = np.argwhere(img > 0)
-    # Initializing accumulator array.
-    # Accumulator array is a 3 dimensional array with the dimensions representing
-    # the radius, X  and Y coordinate .
-    # Also appending a padding of 2 times R_max to overcome the problems of overflow
-    A = np.zeros((max_radius, height + 2 * max_radius, width + 2 * max_radius))
-    # Precomputing all angles
-    theta = np.arange(0, 360) * np.pi / 180
-    for r in range(round(min_radius), round(max_radius)):
-        # Creating a Circle Blueprint
-        bprint = np.zeros((2 * (r + 1), 2 * (r + 1)))
-        (m, n) = (r + 1, r + 1)  # center of blueprint
-        for angle in theta:
-            x = int(np.round(r * np.cos(angle)))
-            y = int(np.round(r * np.sin(angle)))
-            bprint[m + x, n + y] = 1
-        constant = np.argwhere(bprint).shape[0]
-        for x, y in edges:  # For each edge coordinates
-            # Centering the blueprint circle over the edges
-            # and updating the accumulator array
-            # X = [x - m + max_radius, x + m + max_radius]  # Computing the extreme X values
-            # Y = [y - n + max_radius, y + n + max_radius]  # Computing the extreme Y values
-            A[r, x - m + max_radius: x + m + max_radius, y - n + max_radius: y + n + max_radius] += bprint
-        threshold = 7
-        A[r][A[r] < threshold * constant / r] = 0
-    # size to detect peaks
-    region = 15
-    B = np.zeros((max_radius, height + 2 * max_radius, width + 2 * max_radius))
-    # extracting the  circles detected
-    for r, x, y in np.argwhere(A):
-        temp = A[r - region:r + region, x - region:x + region, y - region:y + region]
-        p, a, b = np.unravel_index(np.argmax(temp), temp.shape)
-        B[r + (p - region), x + (a - region), y + (b - region)] = 1
-    circles = np.argwhere(B[:, max_radius:-max_radius, max_radius:-max_radius])
+    rows = img.shape[0]
+    cols = img.shape[1]
+
+    img, _ = edgeDetectionCanny(img, 0.75, 0.2)
+    radius = range(min_radius, max_radius)
+    circles = []
+    threshold = 160
+    for r in radius:
+        print('radius: ', r)
+        acc = np.zeros(img.shape)
+        # Make accumulator
+        for i in range(rows):
+            for j in range(cols):
+                if img[i, j] == 255:
+                    for angle in range(360):
+                        b = j - round(np.sin(angle * np.pi / 180) * r)
+                        a = i - round(np.cos(angle * np.pi / 180) * r)
+                        if 0 <= a < rows and 0 <= b < cols:
+                            acc[a, b] += 1
+
+        if acc.max() > threshold:
+            acc[acc < threshold] = 0
+            # find the circles for this radius
+            for i in range(1, rows - 1):
+                for j in range(1, cols - 1):
+                    if acc[i, j] >= threshold:
+                        avg_sum = acc[i - 1:i + 2, j - 1:j + 2].sum() / 9
+                        if avg_sum >= threshold / 9:
+                            # checking that the distance from every circle to the current circle
+                            # is more than the radius
+                            if all((i - xc) ** 2 + (j - yc) ** 2 > rc ** 2 for xc, yc, rc in circles):
+                                circles.append((i, j, r))
+                                acc[i - r:i + r, j - r:j + r] = 0
     return circles
-
-
-def bilateral_filter_implement(in_image: np.ndarray, k_size: int, sigma_color: float, sigma_space: float) -> (
-        np.ndarray, np.ndarray):
-    """
-    :param in_image: input image
-    :param k_size: Kernel size
-    :param sigma_color: represents the filter sigma in the color space.
-    :param sigma_space: represents the filter sigma in the coordinate.
-    :return: OpenCV implementation, my implementation
-    """
-
-    return
