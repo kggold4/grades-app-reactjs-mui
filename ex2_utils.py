155d5fda5f7ced032ef52cc2a6fdd0873f38be20
kggold4 <68167734+kggold4@users.noreply.github.com>
2022-04-27 06:59:45 +0300

    main update

--- ex2_utils.py
+++ ex2_utils.py
@@ -13,6 +13,14 @@ SIZE_THRESH_RATIO = 0.47
 PI = np.pi
 
 
+def myID() -> np.int:
+    """
+    Return my ID (not the friend's ID I copied from)
+    :return: int
+    """
+    return MY_ID
+
+
 def conv1D(in_signal: np.ndarray, k_size: np.ndarray) -> np.ndarray:
     """
     Convolve a 1-D array with a given kernel
